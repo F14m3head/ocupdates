e0934d55ded99f75877e95dc68cd34fe476da691
Mathieu <matty08ic@gmail.com>
2026-02-06 11:10:45 -0500

    Refactor cog_unload methods to be asynchronous and clean up unnecessary comments; add get_stop_info function for fetching stop details from SQLite DB

--- bot/util/filter_rss.py
+++ bot/util/filter_rss.py
@@ -4,3 +4,21 @@
 # Allow user to chose number of entries to return (up to max)@
 # Returns filtered/sorted list of entries
 # Fetches feed via shared store
+
+
+from typing import List, Dict, Any
+from datetime import datetime
+from bot.services.cache_rss import RSSStore, RSSFeed
+import sqlite3
+
+# Fetch from static SQLite DB from stop_code, return stop_id & stop_name
+def get_stop_info(stop_code: str) -> Dict[str, Any]:
+    con = sqlite3.connect("gtfs_static.db")
+    cur = con.cursor()
+    cur.execute("SELECT stop_id, stop_name FROM stops WHERE stop_code = ?", (stop_code,))
+    row = cur.fetchone()
+    con.close()
+    if row:
+        return {"stop_id": row[0], "stop_name": row[1]}
+    else:
+        return {}
